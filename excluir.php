df47b3c49a6f10fc60e84dfae5c9d1ad13d8f506
Sarah <sarah.l04@aluno.ifsc.edu.br>
2025-06-24 18:03:08 -0300

    Projeto Atualizado

--- excluir.php
+++ excluir.php
@@ -3,12 +3,12 @@ require("conexao.php");
     $id = $_POST['btnApagar'];
 
     $resultado = $pdo->prepare("DELETE FROM produto_caracteristica WHERE id_produto = ?");
-    $resultado->execute($id);
+    $resultado->execute([$id]);
 
     $resultado = $pdo->prepare("DELETE FROM estoque WHERE id_produto = ?");
-    $resultado->execute($id);
+    $resultado->execute([$id]);
 
     $resultado = $pdo->prepare("DELETE FROM produto WHERE id = ?");
-    $resultado->execute($id);
+    $resultado->execute([$id]);
     header("Location: produto.php");
 ?>
